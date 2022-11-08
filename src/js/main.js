e5c3be082b99fd5456b2370adbcdd8947f80fc54
Your Namecl <you@example.com>
2022-11-08 14:44:43 +0100

    changed my styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,50 @@
-let myTodo = ["Fixa TV-bänken", "Rensa kylen och frysen", "Sätta upp en spegel", "Köpa förvaring till skafferiet"];
-
-for(let i = 0; i < myTodo.length; i++){
-
-}
-//Selectors
-let todoInput = document.getElementById("todo-input");
-let todoButton = document.getElementById("todo-btn");
 let todoList = document.getElementById("todo-list");
+let todoBtn = document.getElementById("todo-btn");
 
-//Event listener
-todoButton.addEventListener("click", addTodo);
+function addTodo(todos){
+    let todoInput = document.getElementById("todo-input");
+    new Todo(todoInput.value);
+    console.log(todoInput.value);
+    //Clear todo input value
+    todoInput.value = "";
+}
 
-//Functions
-function addTodo(event){
-    event.preventDefault();
+todoBtn.addEventListener("click", addTodo);
 
-    let todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo__container");
 
-    let newTodo = document.createElement("li");
-    newTodo.classList.add("todo__item");
+//Klass med objekt
+class Todo {
+    constructor(todoname){
+        this.todoname = todoname;
+    }
+}
 
-    todoDiv.appendChild(newTodo);
+let firstTodo = new Todo("Fixa TV-bänken");
+let secoundTodo = new Todo("Rensa kylen och frysen");
+let thirdTodo = new Todo("Sätta upp en spegel");
+let fourthTodo = new Todo("Köpa förvaring till skafferiet");
+let fifthTodo = new Todo("Ta hand om utemöblerna");
+
+let myTodos = [firstTodo, secoundTodo, thirdTodo, fourthTodo, fifthTodo];
+
+//for loop för min todos
+for(let i=0; i < myTodos.length; i++){
+
+    let todoLi = document.createElement("li");
+    todoLi.classList.add("todo__item")
+    todoLi.innerHTML = myTodos[i].todoname;
+
+    todoList.appendChild(todoLi);
 
     let completedBtn = document.createElement("button");
     completedBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
     completedBtn.classList.add("todo__btnCompleted");
-    todoDiv.appendChild(completedBtn);
+    todoList.appendChild(completedBtn);
 
     let deleteBtn = document.createElement("button");
     deleteBtn.innerHTML = '<i class="bi bi-trash3"></i>';
     deleteBtn.classList.add("todo__btnDelete");
-    todoDiv.appendChild(deleteBtn);
+    todoList.appendChild(deleteBtn);
 
-    todoList.appendChild(todoDiv);
-}
+} 
+
